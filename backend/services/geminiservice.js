35f9d9d44a8dea2f0bfbdb5b4e1832e028e8ea23
pranayku01 <132077923+pranayku01@users.noreply.github.com>
2026-04-07 12:09:31 +0530

    feat:changed sdk to @google/genai

--- backend/services/geminiservice.js
+++ backend/services/geminiservice.js
@@ -1,20 +1,16 @@
-const { GoogleGenerativeAI }=require("@google/generative-ai");
+const { GoogleGenAI }=require("@google/genai");
 const { buildCodeAnalysisPrompt }=require("../utils/promptbuilder.js");
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-
-const model = genAI.getGenerativeModel({
-  model: "gemini-3.1-flash-lite-preview",
-});
+const genAI = new GoogleGenAI({apiKey:process.env.GEMINI_API_KEY});
 
 const analyzeCodeService = async ({ code, language }) => {
   try {
     const prompt = buildCodeAnalysisPrompt({ code, language });
 
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
+    const result = await genAI.models.generateContent({model:"gemini-3.1-flash-lite-preview",contents:prompt});
 
-    return response.text();
+
+    return result.text;
   } catch (error) {
     console.error("Gemini Service Error:", error);
     throw new Error("Failed to analyze code");
